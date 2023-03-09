ddb5755f45fc52c1f5b58c52fe23a110228d7415
chiranjib jha <chiranjibjha1999@gmail.com>
2023-03-09 13:28:23 +0530

    class format

--- index.js
+++ index.js
@@ -1,25 +1,31 @@
 const sql=require('mysql2');
 const fs = require('fs');
-const prompt=require("prompt-sync")({sigint:true});
-const dbConfig=JSON.parse(fs.readFileSync('db_config.json'));
-const con=sql.createConnection(dbConfig);
-const keyValue={
-   table:"inserttest",id:"2",name:"cjhs",age:23
-
-};
-function insert(keyValue,con) {
-    con.connect(function(err){
-        if(err) throw err;
-        console.log("Connected");
-    });
-    const querys=`INSERT INTO railway.${keyValue.table}(id,name,age) VALUES(${keyValue.id},"${keyValue.name}",${keyValue.age})`;
-    con.query(querys,(error)=>{
-        if(error){
-            console.error("error",error);
-            return;
-        }
-    });
-
-};
-
-insert(keyValue,con);
+class database{
+    constructor(configFile,tableName){
+        this.dbConfig=JSON.parse(fs.readFileSync(configFile));
+        this.con=sql.createConnection(this.dbConfig);
+        this.tableName=tableName;
+    }
+    insert(keyValuePairFile) {
+        const keyValue=JSON.parse(fs.readFileSync(keyValuePairFile));
+        this.con.connect(function(err){
+            if(err) throw err;
+            console.log("Connected");
+        });
+        const colNames=Object.keys(keyValue).join(",");
+        const value=Object.values(keyValue);
+        const querys=`INSERT INTO railway.${this.tableName}(${colNames}) VALUES(?)`;
+        this.con.query(querys,[value],(error)=>{
+            if(error){
+                console.error("error",error);
+                return;
+            }
+            else{
+                console.log("Record insurted");
+            }
+        });
+    
+    };
+}
+const db=new database("db_config.json","inserttest");
+db.insert("insertKeyValue.json");
